6ee2207f56b4d5a75c0c05ed0890bcd9ed407687
Bennett <bennettcheung@yahoo.com>
2018-08-20 18:09:13 -0700

    Added sections and segmented control to change category

--- InstaKiloSwift/InstaKiloViewController.swift
+++ InstaKiloSwift/InstaKiloViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 private let reuseIdentifier = "InstaKiloCell"
-private var photoArray:[Photo] = [Photo]()
+private var photoArray:[[Photo]] = [[Photo]()]
 
 class InstaKiloViewController: UICollectionViewController {
-  @IBOutlet weak var imageCollectionView: UICollectionView!
+  
+  @IBOutlet weak var categorySegmentedControl: UISegmentedControl!
   
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +24,11 @@ class InstaKiloViewController: UICollectionViewController {
         // Register cell classes
 //        imageCollectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
       
-        setupData()
+        setupDataBySubject()
         
         // Do any additional setup after loading the view.
         DispatchQueue.main.async {
-          self.imageCollectionView.reloadData()
+          self.collectionView?.reloadData()
       }
     }
 
@@ -45,39 +46,89 @@ class InstaKiloViewController: UICollectionViewController {
 
      override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return photoArray.count
     }
 
 
      override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 
-        return photoArray.count
+        return photoArray[section].count
     }
 
      override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! InstaKiloViewCell
       
-      cell.setupCell(photo: photoArray[indexPath.item])
+      cell.setupCell(photo: photoArray[indexPath.section][indexPath.item])
     
         // Configure the cell
     
         return cell
     }
+  
+  override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+    
+    if let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HeaderCell", for: indexPath) as? SectionHeader{
+      
+      let photo = photoArray[indexPath.section][0]
+      if categorySegmentedControl.selectedSegmentIndex == 0{
+        sectionHeader.headerLabel.text = photo.subject
+      }
+      else{
+        sectionHeader.headerLabel.text = photo.location
+      }
+      return sectionHeader
+    }
+    return UICollectionReusableView()
+  }
 
   
-  func setupData(){
+  func setupDataBySubject(){
     photoArray = [
-      Photo.init(withImageName: "travel1", location: "Barcelona", subject: "Sangrada Familia"),
-      Photo.init(withImageName: "travel2", location: "Florence", subject: "Domo"),
-      Photo.init(withImageName: "travel3", location: "Paris", subject: "Effiel Tower"),
-      Photo.init(withImageName: "travel4", location: "Londno", subject: "Big Ben"),
+      [Photo.init(withImageName: "travel1", location: "Barcelona", subject: "Sangrada Familia")],
+      [Photo.init(withImageName: "travel2", location: "Florence", subject: "Domo")],
+      [Photo.init(withImageName: "travel3", location: "Paris", subject: "Effiel Tower")],
+      [Photo.init(withImageName: "travel4", location: "London", subject: "Big Ben")],
+      [Photo.init(withImageName: "travel5", location: "Barcelona", subject: "Parc Guell")],
+      [Photo.init(withImageName: "travel6", location: "Barcelona", subject: "Magic Mountain")],
+      [Photo.init(withImageName: "travel7", location: "Rome", subject: "Trevi Fountain")],
+      [Photo.init(withImageName: "travel8", location: "Florence", subject: "Church")],
+      [Photo.init(withImageName: "travel9", location: "Florence", subject: "Ponte Vecchio")],
+      [Photo.init(withImageName: "travel10", location: "Capri", subject: "Beach")]
+    ]
+  }
+  
+  func setupDataByLocation(){
+    photoArray = [
+      [Photo.init(withImageName: "travel1", location: "Barcelona", subject: "Sangrada Familia"),
       Photo.init(withImageName: "travel5", location: "Barcelona", subject: "Parc Guell"),
-      Photo.init(withImageName: "travel6", location: "Barcelona", subject: "Magic Mountain"),
-      Photo.init(withImageName: "travel7", location: "Rome", subject: "Trevi Fountain"),
+      Photo.init(withImageName: "travel6", location: "Barcelona", subject: "Magic Mountain")],
+      
+      [Photo.init(withImageName: "travel2", location: "Florence", subject: "Domo"),
       Photo.init(withImageName: "travel8", location: "Florence", subject: "Church"),
-      Photo.init(withImageName: "travel9", location: "Florence", subject: "Ponte Vecchio"),
-      Photo.init(withImageName: "travel10", location: "Capri", subject: "Beach")
+      Photo.init(withImageName: "travel9", location: "Florence", subject: "Ponte Vecchio")],
+      
+      [Photo.init(withImageName: "travel3", location: "Paris", subject: "Effiel Tower")],
+      
+      [Photo.init(withImageName: "travel4", location: "London", subject: "Big Ben")],
+      
+      [Photo.init(withImageName: "travel7", location: "Rome", subject: "Trevi Fountain")],
+      
+      [Photo.init(withImageName: "travel10", location: "Capri", subject: "Beach")]
     ]
   }
 
+  @IBAction func categoryChanged(_ sender: Any) {
+    if categorySegmentedControl.selectedSegmentIndex == 0{
+      //sort by subject
+      setupDataBySubject()
+    }
+    else{
+      //sort by location
+      setupDataByLocation()
+      
+    }
+    DispatchQueue.main.async {
+      self.collectionView?.reloadData()
+    }
+  }
 }
---
+++ InstaKiloSwift/SectionHeader.swift
@@ -0,0 +1,14 @@
+//
+//  SectionHeader.swift
+//  InstaKiloSwift
+//
+//  Created by Bennett on 2018-08-20.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+import UIKit
+
+class SectionHeader: UICollectionReusableView {
+        
+  @IBOutlet weak var headerLabel: UILabel!
+}
